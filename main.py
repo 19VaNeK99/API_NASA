0e7b30bf93a1513bf254278629583b149fc6c831
19VaNeK99 <garag-36@yandex.ru>
2020-08-15 15:27:32 +0300

    webbrowser

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import webbrowser
 import json
 key = "wr1ajr3l9XhJqt9EkpfEyZ9ylJk59jqphiJ2KZ4A"
 URL = "https://api.nasa.gov/planetary/apod"
@@ -6,8 +7,11 @@ URL = "https://api.nasa.gov/planetary/apod"
 params = {
     "api_key":key
 }
-
+url_im = ""
 zap = requests.get(URL, params)
 if zap.status_code == 200:
     print(zap.headers['X-RateLimit-Remaining'])
-    print(json.loads(zap.text)["hdurl"])
+    url_im = json.loads(zap.text)["hdurl"]
+
+if url_im:
+    webbrowser.open(url_im, new=2)
